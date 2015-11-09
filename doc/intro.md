208414ff02aabd90311f927ae6c9a857207e7ab6
Konrad Grzanek <kongra@gmail.com>
2015-11-09 15:49:53 +0100

    PDP goes Git!

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to pdp
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,34 @@
+(defproject pdp "0.1.0-SNAPSHOT"
+  :description "PDP backend"
+  :url         "http://127.0.0.1"
+  :license     {:name "Eclipse Public License"
+                :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [criterium           "0.4.3"]]
+  :plugins      [[cider/cider-nrepl   "0.9.1"]]
+
+  :global-vars {*warn-on-reflection* true
+                *print-length*       500}
+
+  :jvm-opts    ["-server"
+
+                "-d64"
+
+                "-Xshare:off"
+                "-XX:+AggressiveOpts"
+                "-XX:+DoEscapeAnalysis"
+                "-XX:+UseCompressedOops"
+                ;; "-XX:+UseNUMA" ;; to check: numactl --hardware
+
+                ;; HEAP SETTINGS
+                "-Xms1G"
+                "-Xmx1G"
+
+                ;; PARALLEL GC SETTINGS
+                "-XX:+UseParallelGC"
+                "-XX:+UseParallelOldGC"
+                "-XX:NewSize=400m"
+                "-XX:MaxNewSize=400m"
+                "-XX:-UseAdaptiveSizePolicy"
+                "-XX:SurvivorRatio=6"])
