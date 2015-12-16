ec347946378d08de33985c59863b235bdf555e06
Konrad Grzanek <kongra@gmail.com>
2015-12-17 00:35:31 +0100

    Made some procs :inline.

--- src/clj/pdp/core/igraph.clj
+++ src/clj/pdp/core/igraph.clj
@@ -8,12 +8,12 @@
 
 ;; CONSTRUCTORS
 
-(defn unigraph
+(defn ^jpdp.igraph.Unigraph unigraph
   [range]
   (jpdp.igraph.Unigraph. range))
 
 
-(defn digraph
+(defn ^jpdp.igraph.Digraph digraph
   [range]
   (jpdp.igraph.Digraph. range))
 
@@ -36,26 +36,30 @@
 
 
 (defn igraph-vertices
+  {:inline (fn [g] `(.vertices ~g))}
   [^jpdp.igraph.IGraph g]
   (.vertices g))
 
 
 (defn igraph-has-vertex?
+  {:inline (fn [g v] `(.hasVertex ~g ~v))}
   [^jpdp.igraph.IGraph g v]
   (.hasVertex g v))
 
 
 (defn igraph-has-edge?
+  {:inline (fn [g v1 v2] `(.hasEdge ~g ~v1 ~v2))}
   [^jpdp.igraph.IGraph g v1 v2]
   (.hasEdge g v1 v2))
 
 
 (defn igraph-successors
+  {:inline (fn [g v] `(.successors ~g ~v))}
   [^jpdp.igraph.IGraph g v]
   (.successors g v))
 
 
-(defn igraph-clone
+(defn ^jpdp.igraph.IGraph igraph-clone
   [^jpdp.igraph.IGraph g]
   (.cloneme g))
 
@@ -63,6 +67,7 @@
 ;; DIGRAPH
 
 (defn digraph-predecessors
+  {:inline (fn [g v] `(.predecessors ~g ~v))}
   [^jpdp.igraph.IDigraph g v]
   (.predecessors g v))
 
@@ -70,21 +75,25 @@
 ;; MUTATIONS
 
 (defn igraph-add-vertex!
+  {:inline (fn [g v] `(.addVertex ~g ~v))}
   [^jpdp.igraph.IMutableGraph g v]
   (.addVertex g v))
 
 
 (defn igraph-remove-vertex!
+  {:inline (fn [g v] `(.removeVertex ~g ~v))}
   [^jpdp.igraph.IMutableGraph g v]
   (.removeVertex g v))
 
 
 (defn igraph-add-edge!
+  {:inline (fn [g v1 v2] `(.addEdge ~g ~v1 ~v2))}
   [^jpdp.igraph.IMutableGraph g v1 v2]
   (.addEdge g v1 v2))
 
 
 (defn igraph-remove-edge!
+  {:inline (fn [g v1 v2] `(.removeEdge ~g ~v1 ~v2))}
   [^jpdp.igraph.IMutableGraph g v1 v2]
   (.removeEdge g v1 v2))
 
@@ -92,7 +101,7 @@
 ;; DUMP/PRESENTATION
 
 (defn igraph-adjacent
-  [g]
+  [^jpdp.igraph.IGraph g]
   (fn [v] (seq (igraph-successors g v))))
 
 
@@ -107,8 +116,8 @@
    (print-graph v (igraph-adjacent g))))
 
 
-;; (def ug (unigraph 100))
-;; (dotimes [i 20] (igraph-add-vertex! ug i))
+(def ^jpdp.igraph.Unigraph ug (unigraph 100))
+(dotimes [i 20] (igraph-add-vertex! ug i))
 
 ;; (def mapka (apply hash-map (take 200 (N))))
 ;; (def secik (apply hash-set (take 0 (N))))
