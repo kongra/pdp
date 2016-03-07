d8b4ef9adcfb332a8f5da59a94ddc22ffcbc6dcc
Konrad Grzanek <kongra@gmail.com>
2016-03-07 13:05:00 +0100

    Moved to the newest Clojure and Cider.

--- project.clj
+++ project.clj
@@ -4,12 +4,14 @@
   :license     {:name "Eclipse Public License"
                 :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies   [[org.clojure/clojure "1.7.0"]
-                   [criterium           "0.4.3"]
-                   [primitive-math      "0.1.3"]
-                   [clongra             "0.1.0-SNAPSHOT"]]
+  :dependencies   [[org.clojure/clojure     "1.8.0"]
+                   [org.clojure/tools.nrepl "0.2.12"]
 
-  :plugins        [[cider/cider-nrepl   "0.9.1"]
+                   [criterium               "0.4.3"]
+                   [primitive-math          "0.1.3"]
+                   [clongra                 "0.1.0-SNAPSHOT"]]
+
+  :plugins        [[cider/cider-nrepl       "0.11.0"]
                    ;; [lein-nodisassemble               "0.1.3" ]
                    ]
 
